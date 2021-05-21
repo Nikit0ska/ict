44c4ca375246d7f1df3160cd3b2afe8eb6d6421d
nikit0ska <nik.ryabov.2014@mail.ru>
2021-05-21 09:16:22 +0300

    B

---
+++ B.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int k, n, a[1000];
+void terms(int x, int y, int z) {
+	if (y == 0) {
+		if (x == k) {
+			for (int i = 0; i < x; i++)
+			{
+				cout << a[i] << " ";
+			}
+			cout << endl;
+		}
+		return;
+	}
+	for (int i = y; i >= z; i--)
+	{
+		if (i >= z)
+		{
+			a[x] = i;
+			terms(x + 1, y - i, i);
+		}
+	}
+}
+int main() {
+	cin >> n >> k;
+	terms(0, n, 1);
+}
